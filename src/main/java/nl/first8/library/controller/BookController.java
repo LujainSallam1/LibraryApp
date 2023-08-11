d23d5dc0a090c593c78d534c0ab44168cca8931b
oadham <omar.adham@first8.nl>
2023-08-11 12:25:58 +0200

    /books can now take query parameter 'isbn'

--- src/main/java/nl/first8/library/controller/BookController.java
+++ src/main/java/nl/first8/library/controller/BookController.java
@@ -4,14 +4,7 @@ import nl.first8.library.domain.Book;
 import nl.first8.library.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.Column;
 import java.util.List;
@@ -25,8 +18,13 @@ public class BookController {
 
 
     @GetMapping("/books")
-    public List<Book> getAll() {
-        return bookRepository.findAll();
+    public List<Book> getAll(@RequestParam(required=false) String isbn) {
+        if(isbn != null){
+            return bookRepository.findByIsbn(isbn);
+        }
+        else {
+            return bookRepository.findAll();
+        }
     }
 
     @GetMapping("/books/{id}")
