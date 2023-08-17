6e3e4426e078a4796413bf9d145ec42d99b429a9
oadham <omar.adham@first8.nl>
2023-08-17 07:41:02 +0200

    removed unused code, minor formatting

--- src/main/java/nl/first8/library/controller/BookController.java
+++ src/main/java/nl/first8/library/controller/BookController.java
@@ -1,33 +1,20 @@
 package nl.first8.library.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import nl.first8.library.controller.exceptions.GoogleBookNotFoundException;
 import nl.first8.library.domain.entity.Book;
-import nl.first8.library.domain.GoogleBookApiResponse;
-import nl.first8.library.repository.BookRepository;
 import nl.first8.library.service.BookAdminService;
 import nl.first8.library.service.BorrowReturnService;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/v1", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_XML_VALUE})
 public class BookController {
-    @Autowired
-    private BookRepository bookRepository;
     @Autowired
     private BorrowReturnService borrowReturnService;
     @Autowired
@@ -35,7 +22,7 @@ public class BookController {
 
     @GetMapping("/books")
     public List<Book> getAll(@RequestParam(required = false) String isbn) {
-       return bookAdminService.getAllBooks(isbn);
+        return bookAdminService.getAllBooks(isbn);
     }
 
     @GetMapping("/books/{id}")
@@ -50,7 +37,6 @@ public class BookController {
         }
     }
 
-
     @PostMapping("/books")
     public ResponseEntity<Book> add(@RequestBody Book book) {
         Book savedBook = bookAdminService.addBook(book);
@@ -62,8 +48,6 @@ public class BookController {
         return bookAdminService.updateBook(id, body);
     }
 
-
-
     @PutMapping("/books/{id}/borrow")
     public ResponseEntity<Book> borrow(@PathVariable(value = "id") Long id) {
         return borrowReturnService.borrow(id);
@@ -71,8 +55,8 @@ public class BookController {
 
     @PutMapping("/books/{id}/handin")
     public ResponseEntity<Book> handin(@PathVariable(value = "id") Long id) {
-      return borrowReturnService.handin(id);
-}
+        return borrowReturnService.handin(id);
+    }
 }
 
 
