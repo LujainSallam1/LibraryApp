1490af596f1303492e0a0e8dcc9e91e423b328a8
oadham <omar.adham@first8.nl>
2023-08-18 08:21:05 +0200

    fix

--- src/main/java/nl/first8/library/service/ScannerService.java
+++ src/main/java/nl/first8/library/service/ScannerService.java
@@ -67,7 +67,6 @@ public class ScannerService {
                     }
                 }
 
-                // Execution flow
                 Long bookId = availableBook.getId();
                 Book book = borrowReturnService.returnBookMember(memberId, bookId);
                 return book; //null if no books were found
